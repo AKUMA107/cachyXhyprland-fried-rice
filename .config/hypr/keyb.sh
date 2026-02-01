267c311382ed3a00cec369f9c1f4106818394c05
daddyaddy <s.ananya.sreekanth@gmail.com>
2026-02-01 17:13:53 +0530

    updated with screenie and wlogout funcs

--- .config/hypr/keyb.sh
+++ .config/hypr/keyb.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# 1. Search the file for the line starting with "primary="
-# 2. Use sed to strip everything except the hex code
 primary_hex=$(grep " on_primary=" .config/hypr/keybcolors.css | cut -d'"' -f2 | sed 's/#//g')
 
-# 3. Check if we actually got a value, then apply it
 if [ -z "$primary_hex" ]; then
     echo "Error: Could not find primary color in keybcolors.css"
     # Fallback color if something goes wrong
